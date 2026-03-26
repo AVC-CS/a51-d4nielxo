ec97bada599e9a60d6f2e9d57a65b22cd9690d6f
d4nielxo <dhaddad3@students.avc.edu>
2026-03-26 10:32:32 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -4,14 +4,28 @@
 
 using namespace std;
 
-int main()
-{
-    // TODO: declare variable n for the exponent input
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    double result = 1.0;
+    
+    if (n >= 0) {
+        for (int i = 0; i < n; i++) {
+            result = result * 2.0;
+        }
+    }
+    else {
+        int times = -n; // |n|
+        for (int i = 0; i < times; i++) {
+            result = result / 2.0;
+        }
+    }
 
-    // TODO: read n from standard input
+    cout << setprecision(5) << fixed;
 
-    // TODO: compute 2 to the power of n using a loop (no cmath pow)
-    // For positive n: multiply result by 2, n times
-    // For negative n: divide result by 2, |n| times
+    cout << "2 to Power " << n << " is " << result << endl;
 
+    return 0;
 }
